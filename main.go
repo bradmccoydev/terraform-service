883d5410c814f80a62f0844d22e8fba09defa09c
Adam Gardner <adam.gardner@dynatrace.com>
2022-01-31 11:05:02 +1000

    Update main.go

--- main.go
+++ main.go
@@ -9,8 +9,6 @@ import (
 
 	cloudevents "github.com/cloudevents/sdk-go/v2" // make sure to use v2 cloudevents here
 	"github.com/kelseyhightower/envconfig"
-	keptnlib "github.com/keptn/go-utils/pkg/lib"
-	keptn "github.com/keptn/go-utils/pkg/lib/keptn"
 	keptnv2 "github.com/keptn/go-utils/pkg/lib/v0_2_0"
 )
 
